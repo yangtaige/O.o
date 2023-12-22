86874b58402397b810316b477ceaed7ece6da33c
wxy000138 <2428599385@qq.com>
2023-12-22 15:27:42 +0800

    practice

--- lec2_practice/Player.py
+++ lec2_practice/Player.py
@@ -6,10 +6,30 @@ import pygame
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, x, y):
-        pass
-
+        super().__init__()
+        self.image = pygame.image.load(GamePath.player)
+        self.image = pygame.transform.scale(self.image,
+    (PlayerSettings.playerWidth, PlayerSettings.playerHeight))
+        self.speed = PlayerSettings.playerSpeed
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+        self.facing = False
     def update(self, keys, scene):
-        pass
+        if keys[pygame.K_w] and self.rect.top > 0:
+            self.rect.y -= self.speed
+        if keys[pygame.K_s] and self.rect.bottom < WindowSettings.height:
+            self.rect.y += self.speed
+        if keys[pygame.K_a] and self.rect.left > 0:
+            self.rect.x -= self.speed
+            if not self.facing:
+                self.image = pygame.transform.flip(self.image, 1, 0)
+                self.facing = True
+        if keys[pygame.K_d] and self.rect.right < WindowSettings.width:
+            self.rect.x += self.speed
+            if self.facing:
+                self.image = pygame.transform.flip(self.image, 1, 0)
+                self.facing = False
 
     def fix_to_middle(self, dx, dy):
-        pass
+        self.rect.x -= dx
+        self.rect.y -= dy
