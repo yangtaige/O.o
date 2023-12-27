172cb51b68ebd575a324506eeb690c0bdb52ddcb
wxy000138 <2428599385@qq.com>
2023-12-27 18:54:29 +0800

    lec3

---
+++ lec3_practice/Map.py
@@ -0,0 +1,48 @@
+# -*- coding:utf-8 -*-
+from Settings import *
+import pygame
+from random import random,randint
+
+class Block(pygame.sprite.Sprite):
+    def __init__(self, image, x, y):
+        super().__init__()
+        self.image = pygame.transform.scale(image,
+        (SceneSettings.tileWidth, SceneSettings.tileHeight))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+
+    def move(self,x,y):
+        self.rect.x += x
+        self.rect.y += y
+
+
+
+def gen_map():
+    images = [pygame.image.load(tile) for tile in GamePath.groundTiles]
+    images = [pygame.transform.scale(image, (SceneSettings.tileWidth, SceneSettings.tileHeight)) for image in images]
+
+    mapObj = []
+    for i in range(SceneSettings.tileXnum):
+        tmp = []
+        for j in range(SceneSettings.tileYnum):
+            tmp.append(images[randint(0, len(images) - 1)])
+        mapObj.append(tmp)
+    
+    return mapObj
+
+def build_obstacles():
+    image = pygame.image.load(GamePath.tree)
+    obstacles = pygame.sprite.Group()
+    midX = SceneSettings.tileXnum // 2
+    midY = SceneSettings.tileYnum // 2
+
+    for i in range(SceneSettings.tileXnum):
+        for j in range(SceneSettings.tileYnum):
+            if random() < SceneSettings.obstacleDensity\
+                and ((i not in range(midX - 3, midX + 3))
+                or (j not in range(midY - 3, midY + 3)))\
+                and (i > midX or j > midY):
+                obstacles.add(Block(image, i * SceneSettings.tileWidth,
+                                  j * SceneSettings.tileHeight))
+    return obstacles
+
---
+++ lec3_practice/NPC.py
@@ -0,0 +1,47 @@
+
+# -*- coding:utf-8 -*-
+
+from Settings import *
+import pygame
+
+# 设置 NPC
+class NPC(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygame.image.load(GamePath.npc)
+        self.image = pygame.transform.scale(self.image, (NPCSettings.npcWidth, NPCSettings.npcHeight))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+
+        self.speed = NPCSettings.npcSpeed
+        self.initialPosition = x
+        self.direction = 1
+        self.patrollingRange = NPCSettings.npcPatrollingRange
+
+        self.talking = False
+        self.talkCD = 0
+        self.clock = 0
+
+    def update(self):
+        if not self.talking:
+            self.rect = self.rect.move(self.speed * self.direction, 0)
+
+            if self.clock == 140:
+                self.direction *= -1
+                self.image = pygame.transform.flip(self.image, True, False)
+                self.clock = 0
+            else:
+                self.clock += 1
+            if self.talkCD > 0:
+                self.talkCD -= 1
+
+    def reset_talk_CD(self):
+        self.talkCD = NPCSettings.talkCD
+
+    def can_talk(self):
+        return self.talkCD == 0
+
+    def move(self,x,y):
+        self.rect.x += x
+        self.rect.y += y
+
