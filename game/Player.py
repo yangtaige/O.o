161da421ae4b7e5f46a5dbbbb72b7413303ac163
yangtg <username@example.com>
2023-12-27 11:19:32 +0800

    Player movement

--- game/Player.py
+++ game/Player.py
@@ -27,20 +27,34 @@ class Player(pygame.sprite.Sprite):
             dy = 0
             if keys[pygame.K_w] and self.rect.top > 0:
                 dy -= self.speed
+                self.index = (self.index + 1) % len(self.images)
+                self.image = self.images[self.index]
+
             if keys[pygame.K_s] and self.rect.bottom < WindowSettings.height:
                 dy += self.speed
+                self.index = (self.index + 1) % len(self.images)
+                self.image = self.images[self.index]
+
             if keys[pygame.K_a] and self.rect.left > 0:
                 dx -= self.speed
                 if not self.facing:
-                    self.image = pygame.transform.flip(self.image, 1, 0)
+                    for i in range(len(self.images)):
+                        self.images[i] = pygame.transform.flip(self.images[i], 1, 0)
                     self.facing = True
+                self.index = (self.index + 1) % len(self.images)
+                self.image = self.images[self.index]
+
             if keys[pygame.K_d] and self.rect.right < WindowSettings.width:
                 dx += self.speed
                 if self.facing:
-                    self.image = pygame.transform.flip(self.image, 1, 0)
+                    for i in range(len(self.images)):
+                        self.images[i] = pygame.transform.flip(self.images[i], 1, 0)
                     self.facing = False
+                self.index = (self.index + 1) % len(self.images)
+                self.image = self.images[self.index]
 
             self.rect = self.rect.move(dx, dy)
+
 
     def render(self, window):
         window.blit(self.image, self.rect)
